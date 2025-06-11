b3ebc8e9335d63462f9572d2da098eaf103cfa79
Mariya Byehan <maria_byehan@yahoo.com>
2025-06-11 13:02:45 +0200

    first commit

---
+++ src/main/java/com/springbootproject/phrasesapi/PhrasesapiApplication.java
@@ -0,0 +1,13 @@
+package com.springbootproject.phrasesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrasesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrasesapiApplication.class, args);
+	}
+
+}
